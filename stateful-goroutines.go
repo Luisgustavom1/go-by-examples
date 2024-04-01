c11a293c5e8f35974d1aeb13f3c06dd78ac5a3d3
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-04-01 09:04:09 -0300

    more tests with stateful goroutines

--- stateful-goroutines.go
+++ stateful-goroutines.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"sync/atomic"
 	"time"
 )
 
 type readOp struct {
-	key  int
-	resp chan int
+	key int
+	res chan int
 }
 
 type writeOp struct {
-	key  int
-	val  int
-	resp chan bool
+	key int
+	val int
+	res chan bool
 }
 
 func main() {
@@ -30,46 +29,47 @@ func main() {
 		for {
 			select {
 			case read := <-reads:
-				read.resp <- state[read.key]
+				read.res <- state[read.key]
 			case write := <-writes:
 				state[write.key] = write.val
-				write.resp <- true
+				write.res <- true
 			}
 		}
 	}()
 
-	for r := 0; r < 100; r++ {
-		go func() {
-			for {
-				read := readOp{
-					key:  rand.Intn(5),
-					resp: make(chan int),
-				}
-				reads <- read
-				<-read.resp
-				atomic.AddUint64(&readOps, 1)
-				time.Sleep(time.Millisecond)
-			}
-		}()
-	}
-
-	for w := 0; w < 10; w++ {
+	for w := 1; w < 100; w++ {
 		go func() {
 			for {
 				write := writeOp{
-					key:  rand.Intn(5),
-					val:  rand.Intn(100),
-					resp: make(chan bool),
+					key: w,
+					val: w * 2,
+					res: make(chan bool),
 				}
 				writes <- write
-				<-write.resp
+				<-write.res
 				atomic.AddUint64(&writeOps, 1)
 				time.Sleep(time.Millisecond)
 			}
 		}()
 	}
 
-	time.Sleep(time.Second)
+	for r := 1; r < 100; r++ {
+		go func() {
+			for {
+				read := readOp{
+					key: r,
+					res: make(chan int),
+				}
+				reads <- read
+				res := <-read.res
+				fmt.Println("reading -> key: ", read.key, "val: ", res)
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second * 10)
 
 	readOpsFinal := atomic.LoadUint64(&readOps)
 	fmt.Println("readOps:", readOpsFinal)
