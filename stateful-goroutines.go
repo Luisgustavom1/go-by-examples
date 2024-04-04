29ed9667a9c2a4483a079c9a75dacf20fde23e64
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-04-04 07:56:27 -0300

    remove for

--- stateful-goroutines.go
+++ stateful-goroutines.go
@@ -37,39 +37,35 @@ func main() {
 		}
 	}()
 
-	for w := 1; w < 100; w++ {
+	for w := 0; w < 100; w++ {
 		go func() {
-			for {
-				write := writeOp{
-					key: w,
-					val: w * 2,
-					res: make(chan bool),
-				}
-				writes <- write
-				<-write.res
-				atomic.AddUint64(&writeOps, 1)
-				time.Sleep(time.Millisecond)
+			write := writeOp{
+				key: w,
+				val: w * 2,
+				res: make(chan bool),
 			}
+			writes <- write
+			<-write.res
+			atomic.AddUint64(&writeOps, 1)
+			time.Sleep(time.Millisecond)
 		}()
 	}
 
-	for r := 1; r < 100; r++ {
+	for r := 0; r < 100; r++ {
 		go func() {
-			for {
-				read := readOp{
-					key: r,
-					res: make(chan int),
-				}
-				reads <- read
-				res := <-read.res
-				fmt.Println("reading -> key: ", read.key, "val: ", res)
-				atomic.AddUint64(&readOps, 1)
-				time.Sleep(time.Millisecond)
+			read := readOp{
+				key: r,
+				res: make(chan int),
 			}
+			reads <- read
+			res := <-read.res
+			fmt.Println("reading -> key: ", read.key, "val: ", res)
+			atomic.AddUint64(&readOps, 1)
+			time.Sleep(time.Millisecond)
 		}()
 	}
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second)
 
 	readOpsFinal := atomic.LoadUint64(&readOps)
 	fmt.Println("readOps:", readOpsFinal)
